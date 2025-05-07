ab7581b4d5c24d75062680175bfebe83772d142f
Rusty-spotted cat <skwjdgh2@hotmail.com>
2025-05-07 15:34:01 +0900

    Add files via upload

---
+++ 250507/Test/src/mypack/Test.java
@@ -0,0 +1,10 @@
+package mypack;
+
+public class Test {
+	public static void main(String[] args)
+	{System.out.println("Welcome!!");
+	System.out.println("자바 세계로 오신 것을 환영합니다.");
+	System.out.println("Sorry~~");
+	System.out.println("재밌어서 미안합니다.~~");
+	}
+}
